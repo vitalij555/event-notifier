c2a01772323866ba929c422b1a48f624c3d19e2b
Vitalij Gotovskij <vitalij@cdveloper.eu>
2020-03-08 10:19:29 +0200

    Fixing setup.py import

--- pypi/setup.py
+++ pypi/setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'event-notifier',         # How you named your package folder (MyLib)
   packages = ['event-notifier'],   # Chose the same as "name"
@@ -10,8 +10,6 @@ setup(
   url = 'https://github.com/vitalij555/event-notifier',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
   keywords = ['EVENT', 'NOTIFY', 'SUBSCRIBE', "OBSERVER"],   # Keywords that define your package best
-  install_requires=[
-      ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
