b99378cdb55cd139f3504dae1f5f1321d0ca8185
Vitalij Gotovskij <vitalij@cdveloper.eu>
2020-03-08 21:16:49 +0200

    Missing description added to pypi

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# basic-notifier
+# event-notifier
+
 Simple python notifier.
+
+## Contents
+
+- [Background](#background)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Constructor](#constructor)
+- [Available Functions](#available-functions)
+- [Contributing](#contributing)
+
+## Background
+
+This is the 
+
+## Installation
+
+```
+pip install event-notifier
+```
+
+## Usage
+
+
+```python
+import Notifier
+
+
+```
+TBD
+Example taken from the internet showing how to mark it down inside .md file:
+The `emit()` function takes three arguments: 
+- an `event` which is a valid CloudEvent,
+- a `path` which is the path associated to the function (default: `/`)
+- a `headers` object that represents the headers sent to the gateway (default: `{"Content-type": "application/json"}`)
+
+The function returns a request object. If your event has a `sync` subscription attached, the `fetch` response will have the status code and body from the subscription. If not, the response will return a `202 Accepted` status code with an empty body.
+
+## Constructor
+
+TBD
+
+**Parameters**
+
+- `url` - `string` - optional, Events API URL, default: `http://localhost:4000`
+- `space` - `string` - optional, space name, default: `default`
+- `configurationUrl` - `string` - optional, Configuration API URL. By default, it's the same as `url` but with `4001` port
+- `connectorUrl` - `string` - optional, Connector API URL. By default, it's the same as `url` but with `4002` port
+- `accessKey` - `string` - optional, access key for hosted Event Gateway. Access key is required for using Configuration API methods on hosted Event Gateway
+
+**Example**
+
+```python
+import Notifier
+
+notifier = Notifier(["onCreate", "onOpen", "onModify", "onDelete"])
+
+```
+
+## Available Functions
+TBD
+
+### some Function Name here
+
+TBD
+
+**Example**
+
+```python
+#TBD
+```
+
+### some other Function Name here
+
+Utility to print the current configuration.
+
+**Example**
+
+```python
+#TBD
+```
+
+
+## Contribute
+TBD
--- pypi/setup.py
+++ pypi/setup.py
@@ -1,19 +1,27 @@
+import setuptools
 from setuptools import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
 setup(
   name = 'event-notifier',         # How you named your package folder (MyLib)
-  packages = ['event-notifier'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  packages=setuptools.find_packages(),   # Chose the same as "name"
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Allows to notify one or many subscribers about something that just happened',   # Give a short description about your library
+  description = 'Event notifier with many subscribers support',   # Give a short description about your library
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'Vitalij Gotovskij',               # Type in your name
   author_email = 'vitalij@cdeveloper.eu',     # Type in your E-Mail
   url = 'https://github.com/vitalij555/event-notifier',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/vitalij555/event-notifier/archive/v_01.tar.gz',
+  # download_url = 'https://github.com/vitalij555/event-notifier/archive/v_0_1_1.tar.gz',
   keywords = ['EVENT', 'NOTIFY', 'SUBSCRIBE', "OBSERVER"],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
+    'Operating System :: OS Independent',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
