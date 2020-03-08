1f9f1b08831ecd84948e01d16b10a729408ccb14
Vitalij Gotovskij <vitalij@cdveloper.eu>
2020-03-09 00:10:11 +0200

    Version update to 0.1.3

--- pypi/setup.py
+++ pypi/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
   name = 'event-notifier',         # How you named your package folder (MyLib)
   packages=setuptools.find_packages(),
-  version = '0.1.2',      # Start with a small number and increase it with every change you make
+  version = '0.1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Event notifier with many subscribers support',   # Give a short description about your library
   long_description=long_description,
